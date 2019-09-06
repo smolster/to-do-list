a7e6fd05ca67cb4af366ae1b73b3520d7f86e639
Swain Molster <swain.molster@gmail.com>
2019-09-06 10:57:43 -0500

    Initial commit

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Swain Molster on 9/6/19.
+//  Copyright © 2019 Swain Molster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
